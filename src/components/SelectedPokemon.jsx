9c318914799606f7daa7b0e4c3fbb6eb61410fcf
Juan Monsalve <j.monsalve@astoundcommerce.com>
2022-04-07 21:28:31 -0500

    fix issue with selectedpokemon

--- src/components/SelectedPokemon.jsx
+++ src/components/SelectedPokemon.jsx
@@ -4,23 +4,25 @@ import PokemonDetails from './PokemonDetails';
 import PokemonPicture from './PokemonPicture';
 
 const SelectedPokemon = ({ pokemonName = 'ditto' }) => {
-  console.log(pokemonName)
   const [pokemon, setPokemon] = React.useState(null);
 
   React.useEffect(() => {
-    get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
-      .then((json) => setPokemon(json));
+    if (pokemonName) {
+      get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`).then((json) =>
+        setPokemon(json)
+      );
+    }
   }, [pokemonName, setPokemon]);
 
   return (
-      <div className='selected-pokemon-container'>
-        {pokemon && (
-          <div>
-            <PokemonPicture pokemon={pokemon} />
-            <PokemonDetails pokemon={pokemon} />
-          </div>
-        )}
-      </div>
+    <div className='selected-pokemon-container'>
+      {pokemon && (
+        <div>
+          <PokemonPicture pokemon={pokemon} />
+          <PokemonDetails pokemon={pokemon} />
+        </div>
+      )}
+    </div>
   );
 };
 
